0ccc6500aef6d5fadc90c7097e29eff1c5714dcf
jeremias23-guapo <jeremiashoy248@gmail.com>
2025-10-30 23:31:15 -0600

    modificando storage

--- backend/config/storage.js
+++ backend/config/storage.js
@@ -1,10 +1,2 @@
 const { Storage } = require('@google-cloud/storage');
-const path = require('path');
-
-const storage = new Storage({
-  projectId: process.env.GCP_PROJECT_ID,
-  keyFilename: path.resolve(__dirname, process.env.GCP_KEYFILE_PATH)
-});
-
-const bucket = storage.bucket(process.env.GCP_BUCKET_NAME);
-module.exports = bucket;
+const storage = new Storage(); // ✅ usa las credenciales del servicio de Cloud Run
